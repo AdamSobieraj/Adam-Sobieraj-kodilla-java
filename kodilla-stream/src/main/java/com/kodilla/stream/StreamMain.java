2f7a8a70bfbf3039a11fc512a2c7fa68830745b8
AdamSobieraj <adam.sobieraj@op.pl>
2019-01-22 20:12:02 +0100

    Mod 7 Zad 1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,46 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+    /*    System.out.println("Welcome to module 7 - Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
+        Processor processor = new Processor();
+
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
+*/
+
+        //  ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+
+      //  processor.execute(executeSaySomething);
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Mod 7 Task 1
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("string to upper", s -> s.toUpperCase() );
+        poemBeautifier.beautify("STRING TO LOWER", s -> s.toLowerCase() );
+        poemBeautifier.beautify("Repl a with b", s -> s.replace("a", "B") );
+        poemBeautifier.beautify("This is text replace", s -> s.replaceFirst("This is", "Can be") );
+    }
+}
