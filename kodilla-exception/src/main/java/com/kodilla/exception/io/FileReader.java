4707e7e1be681623c6c2f6a063879b50d333d104
Sobieraj Adam <soad@jcommerce.pl>
2019-01-30 08:26:58 +0100

    Mod 8 Task 2

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,6 +1,7 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +17,13 @@ public class FileReader {
         System.out.println(file.getPath());
         Path path = Paths.get(file.getPath());
 
-        Stream<String> fileLines = Files.lines(path);
+        try{
+            Stream<String> fileLines = Files.lines(path);
+            fileLines.forEach(System.out::println);
+        }catch (IOException e){
+            System.out.println("Eroro occured" + e);
+        }finally {
+            System.out.println("I'm gona be here.. always");
+        }
     }
 }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,36 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b)/* throws ArithmeticException */{
+   /*     if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+        */
+        double resoult = 0;
+
+       try {
+           resoult = (int)a / (int)b;
+       }catch (ArithmeticException e){
+           System.out.println("Do not devide by 0");
+       }finally {
+           System.out.println("Value a: "+ a+ "  "+"Value b: "+b);
+       }
+        return resoult;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+
+    }
+
+}
