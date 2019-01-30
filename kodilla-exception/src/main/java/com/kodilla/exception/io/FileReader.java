6ff515c46443197213a33e7a6a12d31281e50b50
Sobieraj Adam <soad@jcommerce.pl>
2019-01-30 09:06:38 +0100

    Mod 8 Task 3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -10,18 +10,18 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile(){
+    public void readFile() throws FileReaderException{
     ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
         System.out.println(file.getPath());
-        Path path = Paths.get(file.getPath());
 
         try{
-            Stream<String> fileLines = Files.lines(path);
+            Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
             fileLines.forEach(System.out::println);
         }catch (IOException e){
-            System.out.println("Eroro occured" + e);
+            throw new FileReaderException();
+            //System.out.println("Eroro occured" + e);
         }finally {
             System.out.println("I'm gona be here.. always");
         }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,26 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+import com.sun.deploy.net.FailedDownloadException;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
+     //   FileReader fileReader = new FileReader();
+     //   fileReader.readFile();
+
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+       // FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+
+        try {
+            fileReader.readFile();
+            //fileReaderWithoutHandling.readFile();
+
+        }catch (FileReaderException e){
+            System.out.println("Problem while reading a file");
+        }
     }
 }
