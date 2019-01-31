0df491ce04e533067c4e6fe830aff169aa668bdc
Sobieraj Adam <soad@jcommerce.pl>
2019-01-31 09:31:19 +0100

    Mod 8 Task 3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/Flight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Flight.java
@@ -4,6 +4,11 @@ public class Flight {
     private String departureAirport;
     private String arrivalAirport;
 
+    public Flight(String departureAirport, String arrivalAirport) {
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+    }
+
     public String getDepartureAirport() {
         return departureAirport;
     }
@@ -11,4 +16,24 @@ public class Flight {
     public String getArrivalAirport() {
         return arrivalAirport;
     }
+
+    @Override
+    public boolean equals(Object o){
+        final Flight e = (Flight) o;
+        return this.departureAirport.equals(e.departureAirport) &&
+                this.arrivalAirport.equals(e.arrivalAirport);
+    }
+
+    @Override
+    public int hashCode() {
+        return departureAirport.hashCode()+ arrivalAirport.hashCode();
+    }
+
+    @Override
+    public String toString(){
+        return departureAirport + " from to  " + arrivalAirport ;
+    }
+
+
+
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,4 +1,44 @@
 package com.kodilla.exception.test;
 
-public class RouteNotFoundException {
+import java.util.HashMap;
+import java.util.Map;
+
+public class RouteNotFoundException extends Exception {
+
+    public static void main(String[] args) {
+        Map<Flight,Boolean> tmpMap = new HashMap<>();
+
+        FindFlight findFlight = new FindFlight();
+
+        Flight flight1 = new Flight("Berlin", "Zurich");
+        Flight flight2 = new Flight("Zagrzeb", "Katowice");
+        Flight flight3 = new Flight("Belfast", "Sweeden");
+        Flight flight4 = new Flight("Rome", "Greece");
+
+        tmpMap.put(flight1, true);
+        tmpMap.put(flight2, true);
+        tmpMap.put(flight3, false);
+
+        findFlight.addToMap(tmpMap);
+
+        //Working
+        try{
+            findFlight.findFilght(flight1);
+        }catch (Exception e){
+            System.out.println("Error Occured");
+        }finally {
+            System.out.println("This is the end of task");
+        }
+
+        //Error
+        try{
+            findFlight.findFilght(flight4);
+        }catch (Exception e){
+            System.out.println("Error Occured");
+        }finally {
+            System.out.println("This is the end of task");
+        }
+    }
+
+
 }
