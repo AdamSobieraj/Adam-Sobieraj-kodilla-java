f97a0557471316dd413f6d48ff53e8fed5f7af37
AdamSobieraj <adam.sobieraj@op.pl>
2019-03-09 09:44:53 +0100

    Task1

---
+++ kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/MoviePrinter.java
@@ -0,0 +1,18 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MoviePrinter {
+
+    public static void main(String[] args) {
+
+        MovieStore  movieStore = new MovieStore();
+
+        String moviesToDisplay = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("  _!_  "));
+
+        System.out.println(moviesToDisplay);
+    }
+
+}
