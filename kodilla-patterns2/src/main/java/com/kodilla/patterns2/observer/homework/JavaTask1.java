2db4659d94a9b7c6824fdd017f87ed9e9a3cfbaf
Sobieraj Adam <soad@jcommerce.pl>
2019-05-27 10:04:37 +0200

    'Task4

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/JavaTask1.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns2.observer.homework;
+
+public class JavaTask1 extends Task {
+
+    public JavaTask1() {
+        super("This is task 1");
+    }
+}
---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/JavaTask2.java
@@ -0,0 +1,7 @@
+package com.kodilla.patterns2.observer.homework;
+
+public class JavaTask2 extends Task {
+    public JavaTask2() {
+        super("This is Task 2");
+    }
+}
